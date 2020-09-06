1cd20a69467be259b358ed146daba8205c069dc0
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2020-09-06 23:03:04 +0530

    Redeploying for DB Connection.

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -9,5 +9,12 @@
  *
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
-console.log("PROCESS VARIABLES", process.env)
+console.log("PROCESS VARIABLES:-")
+console.log(process.env.DATABASE_NAME)
+console.log(process.env.DATABASE_PORT)
+console.log(process.env.DATABASE_USERNAME)
+console.log(process.env.DATABASE_PASSWORD)
+console.log(process.env.DATABASE_HOST)
+console.log(process.env.ADMIT_JWT_SECRET)
+console.log(process.env.JWT_SECRET)
 module.exports = () => {};
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '6aa3fbcc-2ff3-42f7-83e9-2d6c45a00f08'
-};
