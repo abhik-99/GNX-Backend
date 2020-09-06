b910d0bb09383e566d19294aef2381d518d3c8fe
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2020-09-07 00:23:16 +0530

    Restarting commit

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -9,14 +9,14 @@
  *
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
-console.log("PROCESS VARIABLES:-")
-console.log(process.env.DATABASE_NAME)
-console.log(process.env.DATABASE_PORT)
-console.log(process.env.DATABASE_USERNAME)
-console.log(process.env.DATABASE_PASSWORD)
-console.log(process.env.DATABASE_HOST)
-console.log(process.env.ADMIN_JWT_SECRET)
-console.log(process.env.JWT_SECRET)
-console.log("PORT", process.env.PORT)
-console.log("HOST", process.env.HOST)
+// console.log("PROCESS VARIABLES:-")
+// console.log(process.env.DATABASE_NAME)
+// console.log(process.env.DATABASE_PORT)
+// console.log(process.env.DATABASE_USERNAME)
+// console.log(process.env.DATABASE_PASSWORD)
+// console.log(process.env.DATABASE_HOST)
+// console.log(process.env.ADMIN_JWT_SECRET)
+// console.log(process.env.JWT_SECRET)
+// console.log("PORT", process.env.PORT)
+// console.log("HOST", process.env.HOST)
 module.exports = () => {};
